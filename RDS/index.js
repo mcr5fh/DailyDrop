15f4b8983803d03618fa18bdded1edf46da4b287
mcr5fh <mcr5fh@virginia.edu>
2018-01-11 22:21:45 -0500

    Change Playlist table to groups. Updated RDS queries accordingly

--- RDS/index.js
+++ RDS/index.js
@@ -1,6 +1,6 @@
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
-'use strict'
-var pg = require("pg")
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+'use strict';
+var pg = require("pg");
 var user = 'root';
 var pwd = 'TuckedIn';
 var dbEndpoint = 'dailydrop.c4noeotwvren.us-east-1.rds.amazonaws.com';
@@ -15,10 +15,13 @@ var rdsCore = require("./rdsCore.js");
 exports.handler = (event, context, callback) => {
 	var client = new pg.Client(conString, function(err, client, done) {
 		if (err) {
-			console.log('error fetching client from pool', err)
+			console.log('error fetching client from pool', err);
 		}
 	});
 
+	// allows for using callbacks as finish/error-handlers
+ context.callbackWaitsForEmptyEventLoop = false;
+  
 	client.connect();
 
 	var query = rdsCore.getSqlQuery(event);
@@ -26,25 +29,49 @@ exports.handler = (event, context, callback) => {
 	console.log("Request: " + JSON.stringify(event));
 	console.log("Query: " + JSON.stringify(query));
 
+	var resultValues = [];
+	var statusCode;
 	client.query(query, (err, result) => {
 		if (err) {
-			console.log(err.stack)
-		} else {
+			console.log("Error executing query");
+			console.log(err.stack);
+			statusCode = 400;
+			resultValues = {
+				"Error detail:" : err.detail,
+				"Schema:" : err.schema,
+				"Table:" : err.table,
+				"Constraint:" : err.constraint
+			};
+		}
+		else {
 			//rows is an array of anomyous objects
-			var res = [];
-			result.rows.forEach(row => res.push(row));
+			result.rows.forEach(row => resultValues.push(row));
 			client.end();
 
-			console.log("Done: " + res);
-			var response = {
-				"statusCode": 200,
+			//The queries will return values inserted/retrieved from the DB
+			//Make sure there was not an error
+			var successfulQuery = rdsCore.validateQueryResults(resultValues);
+
+			if (successfulQuery) {
+				statusCode = 200;
+				console.log("Successful Query. Result: " + resultValues);
+			}
+			else {
+				statusCode = 500;
+				console.log("Error in executing query. Result: " + resultValues);
+			}
+		}
+		client.end();
+		//Status code will be set by now
+		var response = {
+				"statusCode": statusCode,
 				"headers": {
 					"Access-Control-Allow-Origin": "*", // Required for CORS support to work
 				},
-				"body": JSON.stringify(res),
+				"body": JSON.stringify(resultValues),
 				"isBase64Encoded": false
-			}
-			callback(null, response);
-		}
+			};
+		console.log(response);
+		callback(null, response);
 	});
 };
--- RDS/rdsCore.js
+++ RDS/rdsCore.js
@@ -1,50 +1,114 @@
 'use strict';
 
+const GET_SUBMISSIONS_IN_GROUP = '/v1/submission/{groupId}';
+//We should also have one for on a date 
+const GET_SUBMISSIONS_IN_GROUP_ON_DATE = '/v1/submission/{groupId}/{date????}';
+const GET_USERS_IN_GROUP = '/v1/users/{groupId}';
+const GET_GROUPS_FOR_USER = '/v1/users/{userId}/groups';
 
-const GetUsers = '/v1/users/{playlistId}';
-const GetUsersOnPlaylist = '/v1/users/{playlistId}';
+const INSER_USER = '/v1/users';
+const INSERT_GROUP = '/v1/groups';
+const INSERT_SUBMISSION = '/v1/submissions';
 
+const VOTE_ON_SUBMISSION = '/v1/votes';
 
-exports.getSqlQuery = function(event){
+exports.getSqlQuery = function (event) {
 	var resourcePath = event.resourcePath;
 	var httpMethod = event.httpMethod;
+	//Path parameters are used for GETS
+	//Body is used for the updates
 	var pathParams = event.params;
+	var body = JSON.parse(event.body);
 	//Need to decide how these are going to be passed
 	var sqlQuery, sqlValues;
 
-  	console.log('Calling method: ' + resourcePath);
-  	
-  	switch(resourcePath){
-  		//for path /user/
-    	case 'GetUser':
-    		break;
-    	case GetUsersOnPlaylist:
-    		sqlQuery = "SELECT dailydrop.User.UserID, Premium, Name, RefreshToken FROM dailydrop.User \
-				JOIN dailydrop.Playlist_User on \
-				dailydrop.User.UserID = dailydrop.Playlist_User.UserID \
-				WHERE PlaylistID=$1;"
-			sqlValues = [pathParams.playlistId];
-    		break;
+	console.log('Calling method: ' + resourcePath);
+	console.log('Body: ' + body);
+	console.log('PathParams: ' + pathParams);
+
+	//Don't have to check GET/POST becuase API gateway will take care of that
+	switch (resourcePath) {
+		/*
+		 * Gets
+		 */
+		case GET_SUBMISSIONS_IN_GROUP:
+			sqlQuery = "SELECT SongId, UserId, GroupId, SubmissionTime, NumVotes \
+						FROM  dailydrop.Submission \
+						WHERE GroupId=$1 ";
+			sqlValues = [pathParams.groupId];
+			break;
+		case GET_USERS_IN_GROUP:
+			sqlQuery = "SELECT dailydrop.User.UserId, Premium, Name, RefreshToken FROM dailydrop.User \
+						JOIN dailydrop.Group_User on \
+						dailydrop.User.UserId = dailydrop.Group_User.UserId \
+						WHERE GroupId=$1;";
+			sqlValues = [pathParams.groupId];
+			break;
+		case GET_GROUPS_FOR_USER:
+			sqlQuery = "SELECT g.GroupId, Name, VoteTime, SubmissionTime \
+						FROM dailydrop.Group AS g \
+						JOIN dailydrop.Group_User AS gu on \
+						gu.GroupId = g.GroupId \
+						WHERE pu.UserId=$1;";
+			sqlValues = [pathParams.userId];
+			break;
+
     	/*
-    	 *Inserts
+    	 * Inserts
     	 */
-    	case 'InsertUser':
-    		sqlQuery = 'INSERT INTO dailydrop.User(UserID, Premium, Name, RefreshToken) \
-				VALUES($1, $2, $3, $4) RETURNING UserID, Name, RefreshToken;';
-			sqlValues = [pathParams.userId, pathParams.premium, pathParams.name, pathParams.rToken]
+		case INSER_USER:
+			sqlQuery = 'INSERT INTO dailydrop.User(UserId, Premium, Name, RefreshToken) \
+						VALUES($1, $2, $3, $4) \
+						RETURNING UserId, Name, RefreshToken;';
+			sqlValues = [body.userId, body.premium, body.name, body.rToken];
 			break;
-		case 'InsertPlaylist':
-    		sqlQuery = 'INSERT INTO dailydrop.Playlist(UserID, Premium, Name, RefreshToken) \
-				VALUES($1, $2, $3, $4) RETURNING UserID, Name, RefreshToken;';
-			sqlValues = [pathParams.userId, pathParams.premium, pathParams.name, pathParams.rToken]
+		case INSERT_GROUP:
+			//Removed for debugging purposes ON CONFLICT (GroupId) DO NOTHING \
+			sqlQuery = 'INSERT INTO dailydrop.Group(GroupId, Name, VoteTime, SubmissionTime) \
+						VALUES($1, $2, $3, $4) \
+						RETURNING GroupId, Name,  VoteTime, SubmissionTime;';
+			sqlValues = [body.groupId, body.name, body.voteTime, body.submissionTime];
 			break;
-    }
+		case INSERT_SUBMISSION:
+			sqlQuery = "INSERT INTO dailydrop.Submission (SongId, UserId, GroupId, SubmissionTime, NumVotes) \
+						VALUES ($1, $2, $3, CURRENT_TIMESTAMP, 0) \
+						RETURNING SongId, UserId,SubmissionTime, NumVotes;";
+			sqlValues = [body.songId, body.userId, body.groupId];
+			break;
+		/*
+    	 * Updates
+    	 * TODO : is this a POST or GET?
+    	 */
+		case VOTE_ON_SUBMISSION:
+			//Where $1 is the person submitting the vote
+			//Don't let the person vote on their own submission
+			//If Row is non-exsistent, the result will contain zero rows
+			sqlQuery = "UPDATE dailydrop.Submission as s \
+						SET NumVotes = NumVotes + 1 \
+						WHERE s.SongId=$1 \
+						AND s.UserId!=$2 \
+						AND s.GroupId=$3 \
+						RETURNING NumVotes;";
+			sqlValues = [pathParams.songId, pathParams.userId, pathParams.groupId];
+			break;
+	}
 
 	return {
 		text: sqlQuery,
 		values: sqlValues
 	};
-}
+};
 
-
-
+exports.validateQueryResults = function (resultValues) {
+	var successfulQuery = true;
+	if (resultValues.length == 0) {
+		successfulQuery = false;
+	} else {
+		resultValues.forEach(val => {
+			if (val === null || val == "") {
+				successfulQuery = false;
+			}
+		});
+	}
+	return successfulQuery;
+};
