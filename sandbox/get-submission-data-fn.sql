5b31d6b17b49a847335e74c0bedaa57adeab81e6
mcr5fh <mcr5fh@virginia.edu>
2018-03-18 20:09:26 -0400

    Fix function

--- sandbox/get-submission-data-fn.sql
+++ sandbox/get-submission-data-fn.sql
@@ -1,67 +1,31 @@
-
-DROP FUNCTION dailydrop.get_data_for_group(uuid, integer, text);
-
-SELECT * FROM dailydrop.get_data_for_group('4cccf9ee-1633-4cbb-b926-46284219e9cc',1,'date_added');
-
-SELECT * FROM dailydrop.get_data_for_group('4cccf9ee-1633-4cbb-b926-46284219e9cc',1,'num_plays');
-
-CREATE TYPE submission_with_data as 
-(submission_id uuid,
- song_id text,
- song_name text,
- artist_name text,
- submitted_by text,
- num_votes integer,
- num_plays integer,
- date_added timestamp without time zone);
-
-CREATE OR REPLACE function dailydrop.get_data_for_group(
+create or replace function dailydrop.get_data_for_group(
       group_uuid uuid,
       num_days integer,
       sort_column text
 ) returns setof submission_with_data 
-AS
+-- no returns clause necessary, output structure controlled by OUT parameters
+-- returns XXX
+as 
 $BODY$
-DECLARE
+declare
     rec record;
-BEGIN
-FOR rec IN
-   SELECT 
+
+begin
+ for rec in
+   EXECUTE format(
+   'SELECT 
     s.submission_id, s.song_id, s.song_name, s.artist_name, s.user_id,  
-    sm.num_votes, sm.num_plays, s.date_added
+    COALESCE(sm.num_votes,0), COALESCE(sm.num_plays, 0), s.date_added
+
     FROM dailydrop.Submission as s 
     LEFT JOIN dailydrop.Submission_Metrics as sm 
         on s.submission_id = sm.submission_id 
-    WHERE s.group_id = group_uuid
-    and age(now(), s.date_added) < num_days * interval '1 day'
-    ORDER BY quote_ident(sort_column)  DESC
-    LIMIT 10
-LOOP
-    return next rec;
-END LOOP;
+    WHERE s.group_id = %L
+    and age(now(), s.date_added) < %L * interval %L
+    ORDER BY %I DESC', group_uuid, num_days, '1 day',sort_column)
+loop
+        return next rec;
+    end loop;
     -- no return statement necessary, output values already stored in OUT parameters
-END
+end
 $BODY$ language plpgsql;
-
-
-
-
--- These will be used in:
-SELECT
-    (SELECT to_json(array_agg(row))
-     FROM (SELECT
-            (SELECT to_json(array_agg(row))
-             FROM (SELECT *
-                   FROM dailydrop.user t
-                   LIMIT 10) row
-            ) AS day,
-            (SELECT to_json(array_agg(row))
-             FROM (SELECT *
-                   FROM dailydrop.group g
-                   LIMIT 10) row
-            ) AS week) row
-    ) AS popular,
-    (SELECT to_json(array_agg(row))
-     FROM (SELECT *
-           FROM dailydrop.submission) row
-     ) AS subs;
